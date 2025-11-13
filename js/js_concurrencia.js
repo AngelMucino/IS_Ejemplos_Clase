b01e1cb51eb26972a7d5bf07cffd1d9c0d1b5ebc
Angel <angelgabrielmag.cch@gmail.com>
2025-11-13 17:19:03 -0600

    ft: Concurrencia y secuencia

---
+++ js/js_concurrencia.js
@@ -0,0 +1,45 @@
+const out = document.getElementById("output");
+const sec = document.getElementById("sec");
+const conc = document.getElementById("conc");
+
+function log(msg){
+    out.textContent += msg + "\n";
+}
+
+function tarea(nombre, tiempo){
+    return new Promise(res=>{
+        log(`El alumno ${nombre} inicia su tarea`);
+        setTimeout(()=>{
+            log(`El alumno ${nombre} termina su tarea en ${tiempo/1000} s`);
+            res();
+        }, tiempo
+        )
+    });
+}
+
+// secuencial
+sec.onclick = async () =>{
+    out.textContent = "";
+    log("Ejemplo secuencial");
+    const inicio = performance.now();
+    await tarea("Angel", 3000);
+    await tarea("Mike", 2000);
+
+    log(`Tiempo total de ejecucion ${((performance.now() - inicio)/1000).toFixed(2)}s`);
+}
+
+// concurrente
+conc.onclick = async () =>{
+    out.textContent = "";
+    log("Ejemplo concurrente");
+    const inicio = performance.now();
+
+    await Promise.all(
+        [
+            tarea("Sofia", 3000),
+            tarea("Marco", 1000)
+        ]
+    );
+
+    log(`Tiempo total de ejecucion ${((performance.now() - inicio)/1000).toFixed(2)}s`);
+}
